cdb4302b92d8b647f87ee838b9519eea729e62c7
demetregag <demetregagnidze3@gmail.com>
2025-02-24 00:13:24 +0400

    commit

---
+++ lvl 39/homework/task1-50.py
@@ -0,0 +1,197 @@
+# 1
+print("Hello, World!")
+
+# 2
+print(5 + 5)
+
+# 3
+for i in range(3):
+    print(i)
+
+# 4
+import time
+
+# 5
+if True:
+    print("Condition is True")
+
+# 6
+print(len("Python"))
+
+# 7
+x = 10
+
+# 8
+my_list = [1, 2, 3]
+
+# 9
+if False:
+    print("if block")
+else:
+    print("else block")
+
+# 10
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+
+# 11
+my_list = []
+my_list.append("element")
+print(my_list)
+
+# 12
+print("Python"[0])
+
+# 13
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+# 14
+def greet():
+    print("Hello")
+
+greet()
+
+# 15
+print(type(True))
+
+# 16
+try:
+    x = 1 / 0
+except ZeroDivisionError:
+    print("Error")
+
+# 17
+print("Printing")
+
+# 18
+txt = "Hello"
+print(txt[1:4])
+
+# 19
+print(type(123))
+
+# 20
+print(2 in [1, 2, 3])
+
+# 21
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+# 22
+for item in [1, 2, 3]:
+    print(item)
+
+# 23
+def my_func():
+    return "Hello"
+
+# 24
+print(str(123))
+
+# 25
+print(len([1, 2, 3]))
+
+# 26
+if True:
+    pass
+print("Done")
+
+# 27
+print("a" + "b")
+
+# 28
+x = [1, 2, 3] + [4, 5]
+print(x)
+
+# 29
+nums = [10, 3, 7]
+print(min(nums))
+
+# 30
+is_valid = True
+print(is_valid)
+
+# 31
+nums = [1, 2, 3]
+nums.remove(2)
+print(nums)
+
+# 32
+print(type("Hello"))
+
+# 33
+i = 0
+while i < 3:
+    print("Hello")
+    i += 1
+
+# 34
+my_tuple = (1, 2, 3)
+
+# 35
+def add(a, b):
+    return a + b
+
+# 36
+txt = "hello"
+print(txt.upper())
+
+# 37
+x = True and False
+print(x)
+
+# 38
+items = ["a", "b", "c"]
+for i in items:
+    print(i)
+
+# 39
+print(len("გამარჯობა"))
+
+# 40
+if False:
+    print("if")
+elif True:
+    print("elif")
+else:
+    print("else")
+
+# 41
+person = {"name": "John", "age": 30}
+print(person)
+
+# 42
+person = {"name": "John", "age": 30}
+print(person["name"])
+
+# 43
+name = input()
+print(name)
+
+# 44
+print(5 % 2)
+
+# 45
+print(round(3.6))
+
+# 46
+print("<main></main>")
+
+# 47
+print("<div><p>Some text</p></div>")
+
+# 48
+print('<input type="checkbox" />')
+
+# 49
+print('<a href="https://example.com">Click</a>')
+
+# 50
+print('<input type="radio" />')
