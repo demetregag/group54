2ef8b1f0f6a6161a9c11437ffee4406215128507
demetregag <demetregagnidze3@gmail.com>
2025-10-18 19:31:40 +0400

    commit

---
+++ lvl99/classwork/classwork.js
@@ -0,0 +1,4 @@
+const araay =["deme", "gio", "daviti", "dato","dimitri"]
+
+console.log(araay.join("|"))
+console.log(array)
---
+++ lvl99/classwork/classwork2.js
@@ -0,0 +1,4 @@
+const manuaLABS=num => {
+    if (num >= 0) return num
+    else return num*-1
+}
