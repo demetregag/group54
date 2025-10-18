5c417d9a26f51db420f3d5c002046d104d34b93e
demetregag <demetregagnidze3@gmail.com>
2025-10-18 18:29:06 +0400

    commid

---
+++ lvl94/classwork/classwork1.js
@@ -0,0 +1,5 @@
+let words = ["apple", "banana", "avocado", "grape", "keijoa", "cherry", "ananas", "pear", "almond", "kiwi"];
+
+let fie = words.filter(word => word.startsWith("a"));
+
+console.log(fie);A
